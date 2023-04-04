d69a3e1bd5da7407ded158fc27d894da1bba9172
Liam <tatuan.hutech@gmail.com>
2023-04-04 16:33:24 +0700

    be coupon: insert, delete, update, getall, getcoupon

--- app/routers/index.js
+++ app/routers/index.js
@@ -4,12 +4,13 @@ const authRouter = require("./authRoute");
 const productRouter = require("./productRoute")
 const categoryRouter = require("./categoryRoute")
 const brandRouter = require("./brandRoute")
-
+const couponRouter = require("./couponRoute")
 function route(app) {
     app.use('/user', authRouter);
     app.use('/product', productRouter);
     app.use('/category', categoryRouter);
     app.use('/brand', brandRouter);
+    app.use('/coupon', couponRouter);
 }
 
 module.exports = route;
