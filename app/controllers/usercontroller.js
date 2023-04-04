86057e1a451f820f6399c4c5ddc13a40b06064d5
ThuongThai <thai23vn@gmail.com>
2023-04-04 23:40:26 +0700

    be cart: add, delete, get user cart

--- app/controllers/usercontroller.js
+++ app/controllers/usercontroller.js
@@ -242,7 +242,70 @@ const resetPassword = asyncHandler(async (req, res) => {
     await user.save();
     res.json(user);
 });
+// add user cart
+const userCart = asyncHandler(async (req, res) => {
+    const { cart } = req.body;
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+    try {
+        let products = [];
+        const user = await User.findById(_id);
+        // check if user already have product in cart
+        const alreadyExistCart = await Cart.findOne({ orderby: user._id });
+        if (alreadyExistCart) {
+            alreadyExistCart.remove();
+        }
+        for (let i = 0; i < cart.length; i++) {
+            let object = {};
+            object.product = cart[i]._id;
+            object.count = cart[i].count;
+            object.color = cart[i].color;
+            let getPrice = await Product.findById(cart[i]._id).select("price").exec();
+            object.price = getPrice.price;
+            products.push(object);
+        }
+        let cartTotal = 0;
+        for (let i = 0; i < products.length; i++) {
+            cartTotal = cartTotal + products[i].price * products[i].count;
+        }
+        let newCart = await new Cart({
+            products,
+            cartTotal,
+            orderby: user?._id,
+        }).save();
+        res.json(newCart);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
 
+
+//get user cart
+const getUserCart = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+    try {
+        const cart = await Cart.findOne({ orderby: _id }).populate(
+            "products.product"
+        );
+        res.json(cart);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//delete cart
+const emptyCart = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+    try {
+        const user = await User.findOne({ _id });
+        const cart = await Cart.findOneAndRemove({ orderby: user._id });
+        res.json(cart);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
 module.exports = {
     register,
     loginUser,
@@ -257,4 +320,7 @@ module.exports = {
     updatePassword,
     forgotPasswordToken,
     resetPassword,
+    userCart,
+    getUserCart,
+    emptyCart
 };
--- app/routers/authRoute.js
+++ app/routers/authRoute.js
@@ -13,7 +13,10 @@ const {
     logout,
     updatePassword,
     forgotPasswordToken,
-    resetPassword
+    resetPassword,
+    userCart,
+    getUserCart,
+    emptyCart
 } = require("../controllers/usercontroller");
 const {authMiddleware, isAdmin} = require("../../middlewares/authMiddleware");
 
@@ -22,13 +25,16 @@ router.post("/forgot-password-token", forgotPasswordToken);
 router.put("/reset-password/:token", resetPassword);
 router.put('/password', authMiddleware, updatePassword);
 router.post("/login", loginUser);
+router.post("/cart", authMiddleware, userCart);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logout);
+router.get("/cart", authMiddleware, getUserCart);
 router.get("/:id", authMiddleware, getUser);
 router.delete("/:id", deleteUser);
 router.put("/:id", authMiddleware, updateUser);
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
 router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
+router.delete("/empty-cart", authMiddleware, emptyCart);
 router.get("/", getUserList);
 
 
